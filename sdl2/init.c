9e7a3ef9f6bdc5e244be39ef4db1d04ff089369d
Vladyslav Popovych <mailuatc@gmail.com>
2018-01-28 16:21:03 +0200

    Sdl2 normed

--- sdl2/init.c
+++ sdl2/init.c
@@ -18,20 +18,19 @@
 
 void	init_sdl(const char *name)
 {
-    // (void)name;
 	SDL_Init(SDL_INIT_VIDEO);
-    g_sdl.window = SDL_CreateWindow(name, SDL_WINDOWPOS_UNDEFINED,
-                                            SDL_WINDOWPOS_UNDEFINED,
-                                            ww,
-                                            wh,
-                                            0);
-    g_sdl.renderer = SDL_CreateRenderer(g_sdl.window, -1,
-                                            SDL_RENDERER_ACCELERATED);
-    g_sdl.texture = SDL_CreateTexture(g_sdl.renderer,
-                                            SDL_PIXELFORMAT_RGBA8888, 
-                                            SDL_TEXTUREACCESS_STREAMING, 
-                                            ww,
-                                            wh);
-    g_sdl.pixels = malloc(sizeof(uint32_t) * wh * ww);
-    ft_memset(g_sdl.pixels, 0, sizeof(uint32_t) * wh * ww);
+	g_sdl.window = SDL_CreateWindow(name, SDL_WINDOWPOS_UNDEFINED,
+											SDL_WINDOWPOS_UNDEFINED,
+											ww,
+											wh,
+											0);
+	g_sdl.renderer = SDL_CreateRenderer(g_sdl.window, -1,
+											SDL_RENDERER_ACCELERATED);
+	g_sdl.texture = SDL_CreateTexture(g_sdl.renderer,
+											SDL_PIXELFORMAT_RGBA8888,
+											SDL_TEXTUREACCESS_STREAMING,
+											ww,
+											wh);
+	g_sdl.pixels = malloc(sizeof(uint32_t) * wh * ww);
+	ft_memset(g_sdl.pixels, 0, sizeof(uint32_t) * wh * ww);
 }
